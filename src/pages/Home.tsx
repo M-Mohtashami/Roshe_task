8d0e3c4500884ee05daf671def83d2ff3afddb5c
m-mohtashami <m.mohtashami88@gmail.com>
2023-08-22 14:14:58 +0330

    create discount section and add it to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import MainSlider from '../components/MainSlider';
 import CategoriesSection from '../components/CategoriesSection';
 import ProductSection from '../components/ProductSection';
+import ArticleSection from '../components/ArticleSection';
+import DiscountSection from '../components/DiscountSection';
 
 const Home = () => {
   return (
-    <div className="w-full space-y-5">
+    <div className="w-full space-y-10">
       <div className="w-full">
         <MainSlider />
       </div>
       <CategoriesSection />
       <ProductSection />
+      <ArticleSection />
+      <DiscountSection />
     </div>
   );
 };
